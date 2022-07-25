b4f95f0b2ee1a403a0cdeedbb1bddc328dba1f13
dima <dimazaetc0230@gmail.com>
2022-07-25 13:50:10 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import CityWeatherPage from "./components/city-weather-page";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 function App() {
+  console.log(123);
+  console.log(123);
+
+  console.log(123);
+
+  console.log(123);
+
+  console.log(123);
+
   return (
     <Provider store={store}>
       <BrowserRouter>
